784cb0fadad05f7664543e7d6c6c00daacad5045
rfs <remo@mountain-yard.ch>
2023-07-04 11:01:10 +0200

    deps: update dependencies

--- libs/solid/vite.config.ts
+++ libs/solid/vite.config.ts
@@ -1,36 +1,31 @@
 import path from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import {defineConfig} from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 import pkg from "./package.json";
 
 export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    dts({
-      tsConfigFilePath: "tsconfig.build.json",
-      insertTypesEntry: true,
-      noEmitOnError: true,
-      skipDiagnostics: false,
-      logLevel: "info"
-    })
-  ],
-  build: {
-    cssCodeSplit: true,
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      formats: ["es", "cjs"],
-      fileName: format => (format === "es" ? "index.mjs" : "index.cjs")
-    },
-    rollupOptions: {
-      external: [
-        ...Object.keys(pkg.dependencies),
-        ...Object.keys(pkg.peerDependencies),
-        "solid-js",
-        "solid-js/web",
-        "solid-js/store"
-      ]
+    plugins: [
+        solidPlugin({
+            dev: true,
+        }),
+    ],
+    build: {
+        sourcemap: true,
+        cssCodeSplit: true,
+        lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            formats: ["es", "cjs"],
+            fileName: format => (format === "es" ? "index.mjs" : "index.cjs")
+        },
+        rollupOptions: {
+            external: [
+                ...Object.keys(pkg.dependencies),
+                ...Object.keys(pkg.peerDependencies),
+                "solid-js",
+                "solid-js/web",
+                "solid-js/store"
+            ]
+        }
     }
-  }
 });
